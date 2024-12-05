8a87e174b0e4548ed351ebf01c41d6006856d570
lin98 <1726928218@qq.com>
2024-12-05 10:03:47 +0800

    feat:重写 Board 以使用两个循环来制作方块而不是对它们进行硬编码。

--- src/Board.tsx
+++ src/Board.tsx
@@ -39,25 +39,20 @@ export default function Board({xIsNext, squares, onPlay}:{xIsNext:boolean,square
         }
         return null;
       }
-    
+    const boardRowContainer = [0,1,2].map((item,index)=>{
+        return (
+            <div className="board-row" key={index}>
+                {[0,1,2].map((childItem,childIndex)=>{
+                    const targetIndex = item * 3 + childIndex;
+                    return <Square key={childIndex} value={squares[targetIndex]} onSquareClick={() => handleClick(targetIndex)} />
+                })}
+            </div>
+        )
+    })
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            {boardRowContainer}
         </>
     )
 }
